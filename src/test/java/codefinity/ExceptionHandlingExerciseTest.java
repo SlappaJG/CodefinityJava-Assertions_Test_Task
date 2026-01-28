da98b302b9f9fd6bb09a8d1fb48c852847326ffa
Slappa <jgeorgedevelopment@gmail.com>
2026-01-28 00:39:42 -0500

    Task solution

--- src/test/java/codefinity/ExceptionHandlingExerciseTest.java
+++ src/test/java/codefinity/ExceptionHandlingExerciseTest.java
@@ -2,6 +2,9 @@ package codefinity;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 public class ExceptionHandlingExerciseTest {
 
     //ToDo: Pay attention to the test names
@@ -10,21 +13,47 @@ public class ExceptionHandlingExerciseTest {
 
     @Test
     public void checkAge_ShouldThrowIllegalArgumentException_WhenAgeIsLessThan18() {
+        ExceptionHandlingExercise exercise = new ExceptionHandlingExercise();
 
+        int underage = 16;
+
+        IllegalArgumentException error = assertThrows(IllegalArgumentException.class, () -> {
+            exercise.checkAge(underage);
+        });
+
+        assertEquals("Access denied - You must be at least 18 years old.", error.getMessage());
     }
 
     @Test
     public void checkAge_ShouldNotThrowException_WhenAgeIs18OrMore() {
-
+        ExceptionHandlingExercise exercise = new ExceptionHandlingExercise();
+        int ofage = 21;
+        assertDoesNotThrow(() -> exercise.checkAge(ofage));
     }
 
     @Test
     public void printLength_ShouldThrowNullPointerException_WhenStringIsNull() {
+        ExceptionHandlingExercise exercise = new ExceptionHandlingExercise();
+
+        String str = null;
+
+        NullPointerException error = assertThrows(NullPointerException.class, () -> {
+            exercise.printLength(str);
+        });
+
+        assertEquals("String value is null.", error.getMessage());
 
     }
 
     @Test
     public void printLength_ShouldNotThrowException_WhenStringIsNotNull() {
+        ExceptionHandlingExercise exercise = new ExceptionHandlingExercise();
+
+        String str = "test";
+
+        assertDoesNotThrow(() -> {
+            exercise.printLength(str);
+        });
 
     }
 }
